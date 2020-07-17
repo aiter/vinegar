00b8c56001e37daa4a79767692401fabe26e9c0c
乐牙 <liuyunjian@alibaba-inc.com>
2020-07-17 23:58:21 +0800

    init

---
+++ src/main/java/com/jihaoxian/aiter/vinegar/VinegarApplication.java
@@ -0,0 +1,13 @@
+package com.jihaoxian.aiter.vinegar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinegarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinegarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jihaoxian/aiter/vinegar/VinegarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jihaoxian.aiter.vinegar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinegarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
